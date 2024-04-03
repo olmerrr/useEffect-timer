e0f2a647490bbdd2930296b3b45f37f26428f1ad
Yaroslav Dombrovskyi <dombrovskiy91@gmail.com>
2024-04-03 14:37:28 +0200

    refactoring useEffect
    refactoring useEffect

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,4 +1,4 @@
-import { memo, useCallback, useEffect, useState } from 'react';
+import { memo, useEffect, useState } from 'react';
 import clickSound from './ClickSound.m4a';
 
 function Calculator({ workouts, allowSound }) {
@@ -8,15 +8,8 @@ function Calculator({ workouts, allowSound }) {
   const [ durationBreak, setDurationBreak ] = useState(5);
   const [ duration, setDuration ] = useState(0);
 
-  // const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
   const mins = Math.floor(duration);
   const seconds = (duration - mins) * 60;
-
-  // const playSound = useCallback(function () {
-  //     if (!allowSound) return;
-  //     const sound = new Audio(clickSound);
-  //     sound.play();
-  // }, [allowSound])
 
   function handleInc() {
     setDuration(duration => Math.floor(duration) + 1);
@@ -38,6 +31,10 @@ function Calculator({ workouts, allowSound }) {
     }
     playSound();
   }, [allowSound, duration]);
+
+  useEffect(() => {
+    document.title = `Your number ${number} - exercise workout`;
+  }, [number, duration, sets])
   return (
     <>
       <form>
